42ba0e2d0fe4d30e59e3e56f2b4b31b72e82cdd1
Mahdi Dibaiee <mdibaiee@aol.com>
2013-12-05 20:09:42 +0330

    live fix

--- js/main.js
+++ js/main.js
@@ -8,11 +8,14 @@ $(document).ready(function() {
     if( $('#livePrev').hasClass('disabled') ) {
       $('#livePrev').click(livePrev).removeClass('disabled');
     }
+    $('#liveNext').addClass('disabled').off('click', liveNext);
+    
     $('.live li').each(function(i) {
       $(this).animate({'top': parseInt($(this).css('top')) - 100 + 'px'}, 'fast', 'ease', function() {
         var off = $('.live li').last().offset(),
             bot = $('.live ul').offset();
         if( off.top + off.height <= bot.top + bot.height ) $('#liveNext').off('click', liveNext).addClass('disabled');
+        else $('#liveNext').click(liveNext).removeClass('disabled');
       })
     })
   }
@@ -23,11 +26,14 @@ $(document).ready(function() {
     if( $('#liveNext').hasClass('disabled') ) {
       $('#liveNext').click(liveNext).removeClass('disabled');
     }
+    $('#livePrev').addClass('disabled').off('click', livePrev);
+    
     $('.live li').each(function(i) {
       $(this).animate({'top': parseInt($(this).css('top')) + 100 + 'px'}, 'fast', 'ease', function() {
         var off = $('.live li').first().offset(),
             bot = $('.live ul').offset();
         if( off.top >= bot.top ) $('#livePrev').off('click', livePrev).addClass('disabled');
+        else $('#livePrev').click(livePrev).removeClass('disabled');
       })
     })
   }
